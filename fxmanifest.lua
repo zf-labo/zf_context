b35677d8ed5923e493c7f051eaccdfc761a57c00
LeZach <48424078+zf-development@users.noreply.github.com>
2024-07-26 10:48:22 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,6 @@ description 'NoPixel Inspired Context Menu [based on nh-context]'
 version '1.0.2'
 
 client_script 'client/main.lua'
-server_script 'server.lua'
 ui_page 'html/index.html'
 files {
     'html/index.html',
